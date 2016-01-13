b9eb6209001bd122caf9eb1d872075983da6112e
Jesse Riemann <jesse.riemann@gmail.com>
2016-01-13 16:44:01 -0500

    worked on twtt.py

--- twtt.py
+++ twtt.py
@@ -7,7 +7,7 @@ import csv
 INPUT_FILE = 'testdata.manual.2009.06.14.csv'
 
 def strip_html(tweet): # Step 1 of part 1.
-     """
+     """ Remove all html tags and attributes.
      """
      pass
 
@@ -53,20 +53,32 @@ def add_demarcation(tweet): # Step 9 of part 1.
 def normalize_tweet(tweet):
     """ Apply all the steps (1-9) and return the formatted string.
     """
-    pass
+    tweet_class = tweet[0]
+    tweet_text = tweet[-1]
+    
+    tweet_text = strip_html(tweet_text)
+    # Apply all other filters here.
 
 
-def main(input_file, output_file):
-    for line in input_file:
-        normalized_tweet = normalize_tweet(line)
+    return tweet_text + '\n'
+
+
+def main(tweets, output_file):
+    for tweet in tweets: # A tweet is a list of length 6, containing the various fields.
+        normalized_tweet = normalize_tweet(tweet)
         # Write out the file
+        output_file.write(normalized_tweet)
 
 
 if __name__ == "__main__":
     args = sys.argv
     input_file_name = args[1]
     group_id = int(args[2])
-    output_file_name = args[3]
-    main()
+    output_file_name = args[3] 
+    open_input_file = open(input_file_name, 'r')
+    open_output_file = open(output_file_name, 'w+')
+    reader = csv.reader(open_input_file, delimiter=',')
+
+    main(reader, open_output_file)
     print("Done")
     
